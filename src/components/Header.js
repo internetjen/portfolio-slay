84f5adbef1c9e7cfa57acc7b5890a1a416223ddb
Jennifer Arboleda <arboleda.jen@gmail.com>
2023-02-22 19:20:29 -0600

    created components. rendered nav bar (without styling)

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Jennifer Arboleda</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <nav className='nav'>
+            <a href='/' className='portfolio-title'>INTERNETJEN</a>
+            <ul>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#projects'>Projects</a></li>
+                <li><a href='#contact'>Contact</a></li>
+            </ul>
+        </nav>
+    ) 
+}
+
+export default Navigation
