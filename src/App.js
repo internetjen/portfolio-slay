303aac698a3f7e0e326f54ffcbfca17b1d56bc22
Jennifer Arboleda <arboleda.jen@gmail.com>
2023-02-25 12:45:27 -0600

    Added header component to DOM. Fixed styling issues.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from './components/Footer'
 const App = () => 
   <div>
         <Navigation />
-        <Header />
+            <Header />
         {/* <Project />
         <Footer /> */}
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+import profile from '../assets/me_circle.png'
 
-const Header = () => {
+class Header extends Component {
+
+    render() {
     return (
-        <div>
-            <p>Jennifer Arboleda</p>
+        <>
+        <div className='hero-container'>
+            <div className='left-container'>
+                <img src={profile} alt='profile-photo'/>
+            </div>
+
+            <div className='right-container'>
+                <p>Hi, my name is</p>
+                <p>Jennifer Arboleda</p>
+                <p>I am a <span>software engineer</span> living in Chicago.</p>
+                <button className='button hero-button'>Hire me</button>
+            </div>
+            
         </div>
-    )
+        </>
+        ) 
+    }
 }
 
 export default Header
