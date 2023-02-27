ca738a03e956cf63bdae0af8a8eb483db2f61e26
Jennifer Arboleda <arboleda.jen@gmail.com>
2023-02-27 13:16:36 -0600

    Added box styling to portfolio section. Fixed footer styling.

--- src/components/Project.js
+++ src/components/Project.js
@@ -11,48 +11,58 @@ class Project extends Component {
         <>
         <div className='project-container' id='portfolio'>
            <div className='card'>
-                <h2>Project 1</h2>
-                <img className='project-img' src={firstProject} alt='placeholderImg'></img>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <button className='button'>Deployed App</button>
-                <button className='button'>Github </button>
-
+                <div className='project-shadow'>
+                    <h2>Project 1</h2>
+                    <img className='project-img' src={firstProject} alt='placeholderImg'></img>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <button className='button'>Deployed App</button>
+                    <button className='button'>Github </button>
+                </div>
             </div>
             <div className='card'>
-                <h2>Project 2</h2>
-                <img className='project-img' src={secondProject} alt='placeholderImg'></img>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <button className='button'>Deployed App</button>
-                <button className='button'>Github </button>
-
+                <div className='project-shadow'>
+                    <h2>Project 2</h2>
+                    <img className='project-img' src={secondProject} alt='placeholderImg'></img>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <button className='button'>Deployed App</button>
+                    <button className='button'>Github </button>
+               </div>
             </div>
             <div className='card'>
-                <h2>Project 3</h2>
-                <img className='project-img' src={thirdProject} alt='placeholderImg'></img>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <button className='button'>Deployed App</button>
-                <button className='button'>Github </button>
+                <div className='project-shadow'>
+                    <h2>Project 3</h2>
+                    <img className='project-img' src={thirdProject} alt='placeholderImg'></img>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <button className='button'>Deployed App</button>
+                    <button className='button'>Github </button>
+                </div>
             </div>
             <div className='card'>
-                <h2>Project 4</h2>
-                <img className='project-img' src={fourthProject} alt='placeholderImg'></img>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <button className='button'>Deployed App</button>
-                <button className='button'>Github </button>
+                <div className='project-shadow'>
+                    <h2>Project 4</h2>
+                    <img className='project-img' src={fourthProject} alt='placeholderImg'></img>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <button className='button'>Deployed App</button>
+                    <button className='button'>Github </button>
+                </div>
             </div>
             <div className='card'>
-                <h2>Project 5</h2>
-                <img className='project-img' src={secondProject} alt='placeholderImg'></img>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <button className='button'>Deployed App</button>
-                <button className='button'>Github </button>
+                <div className='project-shadow'>
+                    <h2>Project 5</h2>
+                    <img className='project-img' src={secondProject} alt='placeholderImg'></img>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <button className='button'>Deployed App</button>
+                    <button className='button'>Github </button>
+                </div>
             </div>
             <div className='card'>
-                <h2>Project 6</h2>
-                <img className='project-img' src={firstProject} alt='placeholderImg'></img>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <button className='button'>Deployed App</button>
-                <button className='button'>Github </button>
+                <div className='project-shadow'>
+                    <h2>Project 6</h2>
+                    <img className='project-img' src={firstProject} alt='placeholderImg'></img>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <button className='button'>Deployed App</button>
+                    <button className='button'>Github </button>
+                </div>
             </div>
         </div>
         </>
