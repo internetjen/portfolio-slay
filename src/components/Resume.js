82b76f244429723dbfa7a0981333f36e4ca90616
Jennifer Arboleda <arboleda.jen@gmail.com>
2023-02-26 19:44:14 -0600

    updated Resume component to include proficiencies

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,13 +10,29 @@ const Resume = () => {
        link.click();
     }
 
-
     return (
         <>
         <div className='resume-container' id='resume'>
-            <div>
-                <h2>Looking for a resume?</h2>
-                <button className='button resume-button' onClick={handleClick}>Download Here</button>
+            <div className='resume-left'>
+                <div>
+                    <h2>Proficiencies</h2>
+                </div>
+                <div>
+                <i class="fa-brands fa-square-js"></i><p>JavaScript</p>
+                <i class="fa-brands fa-react"></i><p>React</p>
+                <i class="fa-solid fa-database"></i><p>SQL</p>
+                <i class="fa-brands fa-node-js"></i><p>Node.js</p>
+                <i class="fa-brands fa-html5"></i><p>HTML</p>
+                <i class="fa-brands fa-css3-alt"></i><p>CSS</p>
+                </div>
+            </div>
+            <div className='resume-right'>
+                <div>
+                    <h2>Looking for a resume?</h2>
+                </div>
+                <div>
+                    <button className='button resume-button' onClick={handleClick}>Download Here</button>
+                </div>
             </div>
         </div>
         </>
